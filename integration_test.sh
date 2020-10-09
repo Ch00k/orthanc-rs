fd811e69f9c12e49a65e27aa465bbb0a3acbdfe4
Andrii Yurchuk <ay@mntw.re>
2020-10-09 11:38:23 +0200

    Generate coverage report for integration test

--- integration_test.sh
+++ integration_test.sh
@@ -7,4 +7,6 @@ trap cleanup EXIT
 
 ./populate_orthanc.sh
 sleep 2
-RUST_TEST_THREADS=1 cargo test --test integration -- $@
+
+cargo install cargo-tarpaulin
+RUST_TEST_THREADS=1 cargo tarpaulin --lib --verbose --ignore-tests --all-features --workspace --timeout 120 --out Xml
