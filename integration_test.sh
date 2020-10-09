cbb3182701d61964343e03f8a24234b009dc2fa2
Andrii Yurchuk <ay@mntw.re>
2020-10-09 07:59:20 +0200

    Split scripts

--- integration_test.sh
+++ integration_test.sh
@@ -2,26 +2,9 @@
 
 set -e
 
-cleanup() {
-    curl_command="curl"
-    if [ -n $ORC_ORTHANC_USERNAME ] && [ -n $ORC_ORTHANC_PASSWORD ]; then
-        curl_command="$curl_command --user $ORC_ORTHANC_USERNAME:$ORC_ORTHANC_PASSWORD"
-    fi
-
-    patients_curl_command="$curl_command $ORC_ORTHANC_ADDRESS/patients"
-    patients=($($patients_curl_command | jq -c '.[]' | tr -d '"'))
-
-    for patient in "${patients[@]}"; do
-        delete_curl_command="$patients_curl_command/$patient -X DELETE"
-        echo $delete_curl_command
-        $delete_curl_command
-    done
-}
-
+source cleanup_orthanc.sh
 trap cleanup EXIT
 
 ./populate_orthanc.sh
-
 sleep 2
-
 RUST_TEST_THREADS=1 cargo test --test integration -- $@
