40136869066e27ab9a15c45cbe7d74b45da23be3
Andrii Yurchuk <ay@mntw.re>
2021-01-18 21:01:46 +0100

    Make the cleanup script look nicer (#73)

--- scripts/cleanup_orthanc.sh
+++ scripts/cleanup_orthanc.sh
@@ -7,49 +7,25 @@ if ! hash jq 2>/dev/null; then
     exit 1
 fi
 
-cleanup() {
-    curl_command="curl"
-    if [ -n $ORC_ORTHANC_USERNAME ] && [ -n $ORC_ORTHANC_PASSWORD ]; then
-        curl_command="$curl_command --user $ORC_ORTHANC_USERNAME:$ORC_ORTHANC_PASSWORD"
-    fi
+curl_command="curl"
+if [ -n $ORC_ORTHANC_USERNAME ] && [ -n $ORC_ORTHANC_PASSWORD ]; then
+    curl_command="$curl_command --user $ORC_ORTHANC_USERNAME:$ORC_ORTHANC_PASSWORD"
+fi
 
-    patients_curl_command="$curl_command $ORC_ORTHANC_ADDRESS/patients"
-    patients=($($patients_curl_command | jq -c '.[]' | tr -d '"'))
+delete() {
+    address=$1
+    entities=$2
+    list_curl_command="$curl_command $address/$entities"
+    list=($($list_curl_command | jq -c '.[]' | tr -d '"'))
 
-    for patient in "${patients[@]}"; do
-        delete_curl_command="$patients_curl_command/$patient -X DELETE"
+    for e in "${list[@]}"; do
+        delete_curl_command="$list_curl_command/$e -X DELETE"
         echo $delete_curl_command
         $delete_curl_command
     done
-
-    modalities_curl_command="$curl_command $ORC_ORTHANC_ADDRESS/modalities"
-    modalities=($($modalities_curl_command | jq -c '.[]' | tr -d '"'))
-
-    for modality in "${modalities[@]}"; do
-        delete_curl_command="$modalities_curl_command/$modality -X DELETE"
-        echo $delete_curl_command
-        $delete_curl_command
-    done
-
-    peers_curl_command="$curl_command $ORC_ORTHANC_ADDRESS/peers"
-    peers=($($peers_curl_command | jq -c '.[]' | tr -d '"'))
-
-    for peer in "${peers[@]}"; do
-        delete_curl_command="$peers_curl_command/$peer -X DELETE"
-        echo $delete_curl_command
-        $delete_curl_command
-    done
-
-    # Cleanup peer
-    patients_curl_command="$curl_command http://localhost:8029/patients"
-    patients=($($patients_curl_command | jq -c '.[]' | tr -d '"'))
-
-    for patient in "${patients[@]}"; do
-        delete_curl_command="$patients_curl_command/$patient -X DELETE"
-        echo $delete_curl_command
-        $delete_curl_command
-    done
-
 }
 
-cleanup
+delete $ORC_ORTHANC_ADDRESS patients
+delete $ORC_ORTHANC_ADDRESS modalities
+delete $ORC_ORTHANC_ADDRESS peers
+delete $ORC_ORTHANC_PEER_ADDRESS patients
